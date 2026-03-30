15a88dab42346ac21e6e33fa2d79c81c9182b904
LuisCalle17 <nyxluis17@gmail.com>
2026-03-29 20:39:25 -0500

    Realizar Ejercicio 4

---
+++ semana 1/Realizado_ejercicio4.py
@@ -0,0 +1,30 @@
+"""
+Ejercico 4: Escribe un programa que solicite al usuario un número entero y calcule
+si es divisible por 3 y por 5.
+
+"""
+numero = int(input("Ingrese un número entero: "))
+
+if numero % 3 == 0 and numero % 5 == 0:
+    print(f"El número '{numero}' es divisible por 3 y por 5")
+else:
+    print(f"El número '{numero}' no es divisible por 3 y por 5")
+    
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
