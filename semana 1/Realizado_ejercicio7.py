f13998f593a1df02fc239649f58979a945854b60
LuisCalle17 <nyxluis17@gmail.com>
2026-03-31 00:41:17 -0500

    Realizar Ejercicio 7

---
+++ semana 1/Realizado_ejercicio7.py
@@ -0,0 +1,24 @@
+"""
+Ejercico 7: Escribe un programa que solicite al usuario un número entero y
+determine si es par o impar.
+
+"""   
+
+numero = int(input("Ingrese un número entero: "))
+
+if numero % 2 == 0:
+    print(f"El número {numero} es par.")
+else:
+    print(f"El número {numero} es impar.")
+
+
+
+
+
+
+
+
+
+
+
+
