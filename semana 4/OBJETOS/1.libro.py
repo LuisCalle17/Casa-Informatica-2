826e1b3884430d021f389a0407651f4255b65191
LuisCalle17 <nyxluis17@gmail.com>
2026-04-13 18:43:22 -0500

    Clase libro 1

--- semana 4/OBJETOS/1.libro.py
+++ semana 4/OBJETOS/1.libro.py
@@ -1,8 +1,14 @@
 
+class Libro:
+    def __init__(self, titulo, autor):
+        self.titulo = titulo
+        self.autor = autor
+    
+    def mostrar_info(self):
+        print(f"Titulo: {self.titulo} y el autor es: {self.autor}")
 
+libro1 = Libro("Cien años de soledad", "Gabriel García Márquez")
+        
 
-
-
-
-
+libro1.mostrar_info()
 
